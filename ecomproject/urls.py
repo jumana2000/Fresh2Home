7dc68846852ca37c6fa6bb39c594091e61b366a7
Jumana Shaji <jahanashaji20202gmail.com>
2021-11-05 19:17:17 +0530

    Add to cart completed

--- ecomproject/urls.py
+++ ecomproject/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('cart/',include('cart.urls')),
+    path('cart/',include('cart.urls')),
     # path('accounts/',include('accounts.urls')),
     path('',include('shop.urls')),
 ]
