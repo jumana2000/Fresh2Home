fe69e38f6521ff4a49b775d3c31565061f85624e
Jumana Shaji <jahanashaji20202gmail.com>
2021-11-05 17:42:31 +0530

    Superuser created

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from . models import *
 
 # Register your models here.
+
+class categadmin(admin.ModelAdmin):
+    prepopulated_fields={'slug':('name',)}
+
+admin.site.register(categ,categadmin)
+
+class prodadmin(admin.ModelAdmin):
+    list_display=['name','slug','price','stock','available','img']
+    list_editable=['price','stock','img','available']
+    prepopulated_fields={'slug':('name',)}
+admin.site.register(products,prodadmin)
